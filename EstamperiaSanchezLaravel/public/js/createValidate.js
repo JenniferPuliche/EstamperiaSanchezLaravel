653b541eb654834c61f358da4f96570e74fb4774
Belu Ticona <mticona@dc.uba.ar>
2018-12-20 01:02:30 -0300

    [ValidateProduct falta] validar radio

--- EstamperiaSanchezLaravel/public/js/createValidate.js
+++ EstamperiaSanchezLaravel/public/js/createValidate.js
@@ -2,6 +2,14 @@ var Form = document.querySelector('.form-create');
 var Name = document.querySelector('input[name="name"]')
 var Mayorista = document.querySelector('input[name="wholesale_price"]');
 var Minorista = document.querySelector('input[name="retail_price"]');
+var category = document.getElementsByClassName('category').namedItem('div');
+
+Form.onsubmit = function(){
+  if(document.querySelector('input[name="categories[]"]:checked') == null){
+    category.lastChild.innerText ='Debe elegir una categeoría';
+  }
+}
+
 
 Name.onblur = function (){
     if (Name.value == "") {
@@ -23,6 +31,7 @@ Minorista.onblur = function (){
       this.parentNode.querySelector('.invalid-feedback').innerText='Ingrese un precio válido';
     }
   }
+
 
 
 
--- EstamperiaSanchezLaravel/resources/views/product/create.blade.php
+++ EstamperiaSanchezLaravel/resources/views/product/create.blade.php
@@ -68,18 +68,19 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="" class="col-form-label text-md-right">Categorias: </label>
+                            <label for="category" class="col-form-label text-md-right">Categorias: </label>
 
-                            <div class="col-md-12">
+                            <div class="category col-md-12">
                                 @foreach ($categories as $category)
                                     <input id="{{$category->id}}" class="" type="radio" name="categories[]" value="{{$category->id}}">
                                     <label for="{{$category->id}}">{{$category->name}}</label>
                                 @endforeach
 
                                 @if ($errors->has('categories'))
+                                    <span name="cat" class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('categories') }}</strong>
+                                    </span>
                                 @endif
-                                <div class="invalid-feedback"></div>
                             </div>
                         </div>
 
